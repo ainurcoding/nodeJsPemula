758b8638408dbb7c23cbfa361dc241c18a99d07f
ainurcoding <ainurridwan98@gmail.com>
2022-09-20 22:11:34 +0700

    #2 Membuat routing sederhana

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-app.get('/belajar', (req,res) => {
+app.get('/belajar', (req, res) => {
     res.send("Contoh menggunakan GET")
 })
 
-app.listen(PORT, (req,res) => {
+// put : biasa digunakan untuk mengupdate data
+app.put('/belajar', (req, res) => {
+    res.send('Contoh menggunakan put')
+})
+//  post biasa digunakan untuk mengirim data dari form
+app.post('/belajar', (req, res) => {
+    res.send('Contoh menggunakan POST')
+})
+
+// delete
+app.delete('/belajar',(req,res) => {
+    res.send('Contoh menggunakan DELETE');
+})
+
+app.listen(PORT, (req, res) => {
     console.log(`Aplikasi berhasil di jalankan pada port : ${PORT}`)
 })
